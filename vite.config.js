68ec5f194b090b22c9833d70d3aedfd5b3791c75
Igolikov <ivan.golikov@softgamings.com>
2025-05-10 20:00:18 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import laravel from 'laravel-vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3001,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
   },
   plugins: [
+    vue(),
     laravel({
-      input: ['resources/css/inc/default.scss', 'resources/js/app.js'],
+      input: ["resources/css/inc/default.scss", "resources/js/app.js"],
       refresh: true,
     }),
-    vue(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
-      'src': fileURLToPath(new URL('./resources/images', import.meta.url)),
+      "@": fileURLToPath(new URL("./resources/js", import.meta.url)),
+      src: fileURLToPath(new URL("./resources/images", import.meta.url)),
     },
   },
-})
+});
