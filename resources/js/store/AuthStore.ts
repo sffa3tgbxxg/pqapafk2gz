f7308634426ac5590acb786005e90830c5d074dc
Igolikov <ivan.golikov@softgamings.com>
2025-05-11 09:17:07 +0300

    upd

--- resources/js/store/AuthStore.ts
+++ resources/js/store/AuthStore.ts
@@ -4,14 +4,12 @@ import axios from "axios";
 
 export const useAuthStore = defineStore("auth", () => {
   const isAuthenticated = ref(false);
-  const user = ref(null);
   const token = ref(null);
 
   async function login(credentials) {
     try {
       const response = await axios.post("/api/auth/login", credentials);
       isAuthenticated.value = true;
-      user.value = data.user;
       token.value = response.data.token;
       return response.data;
     } catch (error) {
